9032e4b8a21c52795b59c3ceb13e5c5bb9b0d957
ashimodel <49252592+ashimodel@users.noreply.github.com>
2020-04-06 10:39:10 +0700

    gambar

--- web/src/index.js
+++ web/src/index.js
@@ -39,7 +39,6 @@ function MainApp() {
       </ThemeProvider>
   );
 }
-
 ReactDOM.render(<MainApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
