10a0db2283386c7c88ccd5a9538d934b9f0f9fc2
ashimodel <49252592+ashimodel@users.noreply.github.com>
2020-04-21 14:11:52 +0700

    anyar

--- web/src/static/home.js
+++ web/src/static/home.js
@@ -5,5 +5,6 @@ export default function Home() {
 
 	return (<Page>
 		<h1>Welcome to Best Logistic!</h1>
+		<p>Best Logistics Surabaya kami adalah perusahaan yang bergerak dibidang ekspedisi logistic atau jasa pengiriman barang dengan kualitas tenaga kerja serta armada yang professional dengan mengedepankan kualitas, kepuasan dan pelayanan yang optimal kepada pelanggan, anda dapat melihat hasil kerja kami di <link to="/galeri">gallery</link>minat menggunakan dengan jasa kami ? silahkan klik <link to="/order">order</link>atau jika anda ingin bertanya-tanya, silahkan hubungi salah satu "admin" kami</p>
 	</Page>)
 }
