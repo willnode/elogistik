91ec097610a58eceb8e3c09924965d9de8c759fe
Wildan M <willnode@wellosoft.net>
2020-04-27 16:00:22 +0700

    balloon udara

--- web/src/widget/balloon.js
+++ web/src/widget/balloon.js
@@ -10,80 +10,64 @@ import Fab from '@material-ui/core/Fab';
 import IconButton from '@material-ui/core/IconButton';
 import CallIcon from '@material-ui/icons/Call';
 import SendIcon from '@material-ui/icons/Send';
-import WAIcon from '@material-ui/icons/WhatsApp';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import { publicUrl } from '../main/Config';
 import { Context } from '../main/Contexts';
+import { Popper } from '@material-ui/core';
 
 
 const HtmlTooltip = withStyles((theme) => ({
-	tooltip: {
-		backgroundColor: '#111111',
-		color: 'white',
-		maxWidth: 1000,
-		fontSize: theme.typography.pxToRem(12),
-		border: '1px solid #dadde9',
-	},
+  tooltip: {
+    backgroundColor: '#111111',
+    color: 'white',
+    width: '100vw',
+    fontSize: theme.typography.pxToRem(12),
+    border: '1px solid #dadde9',
+  },
 }))(Tooltip);
 
 
 export default function Balloon() {
-	const [open, setOpen] = Context.bind('balloon', useState(false));
-	const ref = React.useRef();
-	return <Box zIndex="3000" position="fixed" bottom={8} right={12}>
-		<HtmlTooltip open={open} arrow interactive
-			PopperProps={{
-				disablePortal: true,
-			}}
-			title={<Box marginTop={2} textAlign="center">
-				<Typography>
-					Contact our Costumer Service
-				</Typography>
-				<img src={publicUrl + "/assets/Chat.png"} alt=""
-					style={{ objectFit: 'contain', margin: '1em 0' }} width="100%" height="250px" />
-				<Paper component="form" action="https://wa.me/6287825119580">
-					<InputBase
-						inputRef={ref}
-						name="text"
-						placeholder="Tanyakan ke kami"
-						required
-						autoComplete="none"
-					/>
-					<IconButton type="submit" aria-label="submit">
-						<SendIcon />
-					</IconButton>
-				</Paper>
-				<p>
-					Anda bisa tanyakan lebih lanjut dengan menghubungi kami langsung
-				</p>
-				<ButtonGroup color="secondary">
-					<Button variant="text">
-						<Box clone marginRight={1}><WAIcon /></Box>
-					</Button>
-					{
-						Object.entries({
-							Hamz: '6287825119580',
-							Desy: '6282334573264',
-							Atha: '6282244024042',
-						}).map(([name, hp]) =>
-							<Button component="a" key={name} target="_blank" rel="noopener noreferrer"
-								href={`https://wa.me/${hp}?text=Halo+Best+Logistic,+Saya+Ingin+Menanyakan+Sesuatu,+Dapatkah+Anda+Membantu+Saya`}>
-								{name}
-							</Button>
-						)
-					}
-				</ButtonGroup>
-			</Box>}
-		>
-			<Fab
-				variant="extended"
-				onClick={() => setOpen(!open)}
-				color='secondary'
-			>
-				<Box clone marginRight={1}><CallIcon /></Box> Chat
-			</Fab>
-		</HtmlTooltip>
-
-	</Box>
+  const [open, setOpen] = Context.bind('balloon', useState(false));
+  const anchorEl = React.useRef();
+  const ref = React.useRef();
+  return <div className="cs-float">
+    <Fab
+      variant="extended"
+      onClick={() => setOpen(!open)}
+      color='secondary'
+      buttonRef={ref}
+    >
+      <CallIcon /> Chat
+		</Fab>
+    <Popper disablePortal open={open}>
+      <div className="cs-container">
+        <Typography className="cs-title">
+          Contact our Costumer Service
+			  </Typography>
+        <Grid container>
+          {
+            [
+              ['Benny', '6281232221008', 'Heavy Cargo & Project', 'benny'],
+              ['Hamz', '6281380046039', 'Retail Cargo Maluku & Papua', 'ilham'],
+              ['Riza', '62895335572238', 'Retail Cargo Jawa & Kalimantan', 'riza'],
+              ['Desy', '6281380046038', 'Retail Cargo NTB & NTT', 'dessy'],
+              ['Widda', '6283831901981', 'Retail Cargo Bali & Sulawesi', 'widda'],
+            ].map(([name, hp, title, img], i) =>
+              <Grid key={hp} item xs={i < 2 ? 6 : 4}>
+                <div className="cs-item">
+                  <img className="cs-avatar" src={`/assets/CHAT/${img}.png`} alt={name} />
+                  <div className="cs-name">{name}</div>
+                  <Button component="a" className="title-name" key={name} target="_blank" rel="noopener noreferrer" href={`https://wa.me/${hp}?text=Halo+Best+Logistic,+Saya+Ingin+Menanyakan+Sesuatu,+Dapatkah+Anda+Membantu+Saya`} color="secondary" variant="contained">
+                    {title}
+                  </Button>
+                </div>
+              </Grid>
+            )
+          }
+        </Grid>
+      </div>
+    </Popper>
+  </div>
 }
